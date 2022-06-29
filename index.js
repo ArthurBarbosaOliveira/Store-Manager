1868777f400d7f58f5f7028c6baae500f9f86884
Arthur <arthurbarbosa93@gmail.com>
2022-06-29 16:41:42 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
+// começando
+
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
